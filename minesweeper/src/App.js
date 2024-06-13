2fec464b6b1009f5d1bda60199bb66aa44011960
Grobsie <cruicer1@gmail.com>
2024-06-13 14:42:55 +0200

    added clickable tiles

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 function NavigationPanel() {
   return (
     <>
@@ -10,6 +12,16 @@ function NavigationPanel() {
   )
 }
 
+function TitlePanel() {
+  return (
+    <>
+      <div class="titlepanel">
+        <h1>minesweeper</h1>
+      </div>
+    </>
+  );
+}
+
 function InfoPanel() {
   return (
     <>
@@ -20,46 +32,172 @@ function InfoPanel() {
   );
 }
 
-function TilePanel() {
-  const array = [
-                0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0,
-                ]
-  const cellsToReturn = [];
-  for (let i in array) {
-    console.log(i + " is equal to 0: " + (i==0))
-    cellsToReturn.push(Tile(i));
-    if (i % 5 === 0 && i > 1) {
-      cellsToReturn.push("<br></br");
-    }
-  };
+function TimerPanel() {
   return (
     <>
-    <div class="tilePanel">
-      {cellsToReturn}
+      <div class="timerpanel">
+        12m55s
+      </div>
+    </>
+  );
+}
+
+function TilePanel() {
+  const array = [
+               "B" , 1  ,  0  , 1  , 1  ,
+                1  , 1  ,  0  , 1  ,"B" ,
+                0  , 0  ,  0  , 1  , 1  ,
+                1  , 1  ,  2  , 2  , 2  ,
+                1  ,"B" ,  2  ,"B" ,"B" ,
+                ]
+  const returnArray = [];
+  let tempCellsInRow = [];
+  let counter = 0;
+  let rowLength = Math.sqrt(array.length);
+  for (let rows = 0; rows < rowLength; rows++) {
+    for (let columns = 0; columns < rowLength; columns++) {
+      tempCellsInRow.push(Tile(array[counter]));
+      counter++;
+    }
+    returnArray.push(Row(tempCellsInRow));
+    tempCellsInRow = [];
+  };
+
+  return (
+    <>
+    <div class="tilepanel">
+      {returnArray}
     </div>
     </>
   );  
 }
 
-function Tile(value) {
+function Row(cellsInRow) {
   return (
     <>
-      <div class="tile">{value}</div>
+      <div class="row">{cellsInRow}</div>
     </>
-  )
+  );
 }
+
+function Tile(value) {
+  const [val, setValue] = useState(null);
+  
+  function handleLeftClick() {
+    if (val === null) {
+      setValue(value);
+    }
+  }
+
+  function handleRightClick(e) {
+    e.preventDefault(); //prevent default context menu
+    if (val === null) {
+      setValue("flag");
+    } else if (val === "flag") {
+      setValue(null);
+    }
+    
+  }
+
+  return (
+    <>
+      <div class="tile" onClick={handleLeftClick} onContextMenu={handleRightClick}>{val}</div>
+    </>
+  );
+}
+
+function LeaderboardPane() {
+  return (
+    <>
+      <div class="leaderbordpanel">
+        <table>
+          <tr>
+            <th>Leaderbord</th>
+          </tr>
+          <td>
+            <th>Easy</th>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+          </td>
+          <td>
+          <th>Medium</th>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+          </td>
+          <td>
+          <th>Hard</th>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+              <tr>
+                <td>winner1</td>
+                <td>31s</td>
+              </tr>
+          </td>
+        </table> 
+      </div>
+    </>
+  );
+}
+
 export default function Game() {
   return (
     <>
+      <TitlePanel />
       <NavigationPanel />
       <InfoPanel />
+      <TimerPanel />
       <TilePanel />
-
-    
+      <LeaderboardPane />
     </>
   );
 }
